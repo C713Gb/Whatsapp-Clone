8252b83c24682bb8f3b4d6707bb99043fd50a8f8
Gourav Banerjee <39580718+C713Gb@users.noreply.github.com>
2020-08-18 09:03:32 +0530

    initial commit

---
+++ app/src/main/java/com/example/whatsappclone/RegisterActivity.java
@@ -0,0 +1,20 @@
+package com.example.whatsappclone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+
+    @Override
+    public void onBackPressed() {
+        finishAffinity();
+
+    }
+}
--- app/src/main/java/com/example/whatsappclone/StartActivity.java
+++ app/src/main/java/com/example/whatsappclone/StartActivity.java
@@ -2,13 +2,37 @@ package com.example.whatsappclone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
 
 public class StartActivity extends AppCompatActivity {
+
+    private static int WELCOME_TIMEOUT = 4000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent welcome = new Intent(StartActivity.this, RegisterActivity.class);
+                startActivity(welcome);
+                finish();
+            }
+        }, WELCOME_TIMEOUT);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
+
     }
 }
